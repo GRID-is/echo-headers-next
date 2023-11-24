e4e97c7455faa32c014a538d6ceacc9ab0082b6a
Eiríkur Fannar Torfason <eirikur@grid.is>
2023-11-24 11:12:05 +0000

    Get to a working repro

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    const buffer = new Uint32Array(2);
+    const nonce = 'asdf';
+    return [
+      {
+        source: '/',
+        headers: [
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'same-origin',
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,12 +1,17 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import NextDocument, { Html, Head, Main, NextScript, DocumentContext } from 'next/document'
 
-export default function Document() {
+Document.getInitialProps = async (ctx: DocumentContext) => {
+  const initialProps = await NextDocument.getInitialProps(ctx)
+  return { ...initialProps, nonce: ctx.req?.headers['x-nonce'] };
+}
+
+export default function Document({nonce}: {nonce: string | undefined}) {
   return (
     <Html lang="en">
-      <Head />
+      <Head nonce={nonce} />
       <body>
         <Main />
-        <NextScript />
+        <NextScript nonce={nonce} />
       </body>
     </Html>
   )
