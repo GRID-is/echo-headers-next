e73ad4f1441fec7c16651c320c4439cc61e95a70
Eiríkur Fannar Torfason <eirikur@grid.is>
2023-11-24 12:35:53 +0000

    Simplify further

--- next.config.js
+++ next.config.js
@@ -2,16 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   async headers() {
-    const buffer = new Uint32Array(2);
-    const nonce = 'asdf';
     return [
       {
         source: '/',
         headers: [
-          {
-            key: 'Permissions-Policy',
-            value: 'geolocation=(),midi=(),sync-xhr=(),microphone=(),camera=(),magnetometer=(),gyroscope=(),fullscreen=(self),payment=()',
-          },
           {
             key: 'Referrer-Policy',
             value: 'same-origin',
